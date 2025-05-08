c92402500878c09c5c8c1462ea85c1c08c02f17e
decosawa <andreluiiz.gst@gmail.com>
2025-05-08 19:13:35 -0300

    Initiliazing the repo

---
+++ src/main/java/com/mycompany/projetopoo/Principal.java
@@ -0,0 +1,12 @@
+package com.mycompany.projetopoo;
+
+/**
+ *
+ * @author andre
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
